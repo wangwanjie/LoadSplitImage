097a5a79bf2702076eb920b172e40889a92a99be
VanJay <wangwanjie1993@gmail.com>
2019-04-10 16:34:50 +0800

    Initial commit

---
+++ LoadSplitImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoadSplitImage
+//
+//  Created by VanJay on 2019/4/10.
+//  Copyright © 2019 VanJay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoadSplitImage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LoadSplitImage
+//
+//  Created by VanJay on 2019/4/10.
+//  Copyright © 2019 VanJay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
